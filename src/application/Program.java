e28cc9ef1635cd0730b553ebf74b013130b75de9
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-12 17:16:33 +0100

    feat: implement findById seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,11 +8,12 @@ import model.entities.Department;
 import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
-		Department obj =  new Department(1, "Books");
+	
 		
-		//public Seller(Integer id, String name, Date birthDate, Double baseSalary, Department department)
-		Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.0, obj);
-		SellerDao sellerDao =  DaoFactory.createSellerDao();
+	
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller  = sellerDao.findById(3);
 		
+		System.out.println(seller);
 	}
 }
