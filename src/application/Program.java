5a2a8be906d49ced94bb37ce81a24c5fc2299537
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-12 15:17:36 +0100

    application package and Program class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,12 @@
 package application;
 
-public class Program {
+import model.entities.Department;
 
+public class Program {
+	public static void main(String[] args) {
+		Department obj =  new Department(1, "Books");
+		
+		System.out.println(obj);
+		
+	}
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import com.mysql.cj.xdevapi.DbDocFactory;
+
 
 import java.sql.Statement;
 
