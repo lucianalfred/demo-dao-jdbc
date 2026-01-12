95380ed698ff2a7864e9c0a65650f2684368a86b
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-12 15:05:59 +0100

    Model Entity for Department

---
+++ src/model/entities/Department.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+public class Department {
+	
+	private Integer id;
+	private String name;
+	
+	public Department(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+
+}
